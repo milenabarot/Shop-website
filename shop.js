28320174bd39a6dfd0088953f907738ec6fc0a66
milenabarot <milenabarot@gmail.com>
2020-08-07 16:00:49 +0100

    Image grid, and JS for hamburger menu button
    Started to design an image grid using CSS. Began to incorporate JS for the hamburger menu (edited html & css code for this too); position property,  added in extra classes to style hamburger menu. JS: function for hamburer menu button, query selectors to display menu when button is clicked and hide it when not.

--- shop.js
+++ shop.js
@@ -1,3 +1,33 @@
-const clickButton = function () {
-    console.log('The button has been clicked');
+const clickButton = function (buttonType) {
+    console.log(`The ${buttonType} button has been clicked`);
+}
+
+/*
+const hamburgerMenu = function (event) {
+    const blob = event.target;
+    console.log(blob);
+    if (blob.style.margin === '10px') {
+        blob.style.margin = '0px';
+    } else {
+        blob.style.margin = '10px';
+    }
+
+
+}
+*/
+
+const onHamburgerButtonClick = function () {
+    const hamburgerMenu = document.querySelector('#hamburger-menu');
+    if (hamburgerMenu.style.visibility === 'hidden') {
+        hamburgerMenu.style.visibility = 'visible';
+    } else {
+        hamburgerMenu.style.visibility = 'hidden';
+    }
+    const hamburgerButton = document.querySelector('#hamburger-button');
+    if (hamburgerButton.className === 'hamburger-button') {
+        hamburgerButton.className = 'hamburger-button hamburger-is-active';
+    } else {
+        hamburgerButton.className = 'hamburger-button';
+    }
+    console.log(hamburgerButton);
 }
